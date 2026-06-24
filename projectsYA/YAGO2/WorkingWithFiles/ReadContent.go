8217a4e33f2d0240a8e1b83abed8d34cde056b56
Dmitry <tele1056@gmail.com>
2026-06-24 21:07:49 +0700

    Просто нужно было применить .ReadFile

--- projectsYA/YAGO2/WorkingWithFiles/ReadContent.go
+++ projectsYA/YAGO2/WorkingWithFiles/ReadContent.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
 	//"io"
 	"os"
-	"strings"
+	//"strings"
 )
 
-//тут по заданию функция принмает на вход путь к файлу и возвращает его содержимое, то есть нам нужно прочитать его и буффер перевести в стринг и вернуть.
-// т.к. мы не знаем заранее какой файл будет передаваться, то тут будем читать по байтам, т.е. в цикле (for) читаем по опред. колич. байт
 func ReadContent(filename string) string {
-	//первым делом на надо открыть файл (который передадут в випути стринг формата)
-	file, err := os.Open(filename) //тут получили объект типа *os.File который можно читать
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		return ""
 	}
-	//и закрываем файл используя defer, и если мы записываем, то исп. вот такой defer:
-	// defer func() {
-  	// 	errClose := f.Close() 
-		// if errClose != nil { //т.к только тут можно обработать ошибку
-
-		// }
-	// }()
-	defer file.Close()
-
-	//теперь читаем, методом "считывать файл построчно"
-	fileScanner := bufio.NewScanner(file) // создаем сканер
-
-	var builder strings.Builder // создаем билдер - в него будем собирать строки со сканера
-	for fileScanner.Scan() {
-		builder.WriteString(fileScanner.Text())
-		builder.WriteString("\n")
-	}
-
-	//обрабатываем ошибку сканера
-	if err := fileScanner.Err(); err != nil {
-		return ""
-	}
-
-	//возвращаем итог (билдер)
-	return builder.String()
+	return string(data)
 }
 
 func main() {
